83498feaa205ecf411588e45185b8712fce22474
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-03-24 22:09:49 +0000

    added cache control

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -200,6 +200,11 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'molnarlin-boutique-ado'
     # change this to your AWS bucket name
