8828d9c611c5a4faecc0cb55dbab95c9b2485b2a
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2025-03-24 12:40:09 +0000

    deployment to heroku

--- boutique_ado_v1/settings.py
+++ boutique_ado_v1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -33,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'molnarlin-boutique-ado-app-0448433bf966.herokuapp.com',
 ]
 
 
@@ -113,12 +115,17 @@ WSGI_APPLICATION = 'boutique_ado_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
